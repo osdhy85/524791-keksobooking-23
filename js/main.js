328102a023f888d07e5df0d7fa60c3fba88b7e43
Pavel Osadchiy <osdhy85@gmail.com>
2021-05-30 21:31:50 +0300

    Добавил вспомогательные ф-ции для возврата целых и дробных чисел из положительного диапазона

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function getRandomWholeNum (randomMinNum, randomMaxNum) {
+  if (randomMinNum < randomMaxNum && randomMinNum >= 0) {
+    randomMinNum = Math.ceil(randomMinNum);
+    randomMaxNum = Math.floor(randomMaxNum);
+    return Math.floor(Math.random() * (randomMinNum - randomMaxNum + 1)) + randomMaxNum;
+  }
+  return 'Error';
+}
+
+function getRandomFractionNum (randomMinNum, randomMaxNum, digits) {
+  if (randomMinNum < randomMaxNum && randomMinNum >= 0 && digits >= 0) {
+    return (Math.random() * (randomMinNum - randomMaxNum) + randomMaxNum).toFixed(digits);
+  }
+  return 'Error';
+}
+
+console.log(getRandomWholeNum(10, 45));
+console.log(getRandomFractionNum(14.34, 32.21, 3));
