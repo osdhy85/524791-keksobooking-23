9f2b9fbe50348cc7077d696bf70f46ba63100b25
Pavel Osadchiy <osdhy85@gmail.com>
2021-06-09 20:40:10 +0300

    домашка 4.9. исправления (09_06_21)

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
+const templatePhotos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const propertyTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const timeType = ['12:00', '13:00', '14:00'];
+const featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
 function getRandomWholeNum(randomMinNum, randomMaxNum) {
   if (randomMinNum < randomMaxNum && randomMinNum >= 0) {
     randomMinNum = Math.ceil(randomMinNum);
     randomMaxNum = Math.floor(randomMaxNum);
-    return (
-      Math.floor(Math.random() * (randomMinNum - randomMaxNum + 1)) +
-      randomMaxNum
-    );
+    return Math.floor(Math.random() * (randomMaxNum - randomMinNum + 1)) + randomMinNum;
   }
   return 'Error';
 }
 
 function getRandomFractionNum(randomMinNum, randomMaxNum, digits) {
   if (randomMinNum < randomMaxNum && randomMinNum >= 0 && digits >= 0) {
-    return (
-      Math.random() * (randomMinNum - randomMaxNum) +
-      randomMaxNum
-    ).toFixed(digits);
+    return (Math.random() * (randomMaxNum - randomMinNum) + randomMinNum).toFixed(digits);
   }
   return 'Error';
 }
@@ -23,10 +27,61 @@ function getRandomFractionNum(randomMinNum, randomMaxNum, digits) {
 // getRandomWholeNum(10, 45);
 // getRandomFractionNum(14.34, 32.21, 2);
 
-function getUsersList(numberUsers) {
+function generatePhotos() {
+  const photos = [];
+  for (let index = 0; index < templatePhotos.length; index++) {
+    if (getRandomWholeNum(0, 1)) {
+      photos.push(templatePhotos[index]);
+    }
+  }
+  return photos;
+}
+
+function generateRandomArrayItem(array) {
+  const randomIndex = getRandomWholeNum(0, array.length - 1);
+  return array[randomIndex];
+}
+
+function generateFeatures() {
+  const features = [];
+  for (let index = 0; index < featuresList.length; index++) {
+    if (getRandomWholeNum(0, 1)) {
+      features.push(featuresList[index]);
+    }
+  }
+  return features;
+}
+
+function generateUser() {
+  const location = {
+    lat: getRandomFractionNum(35.65000, 35.70000, 5),
+    lng: getRandomFractionNum(139.70000, 139.80000, 5),
+  };
+  return {
+    author: { avatar: `img/avatars/user0${getRandomWholeNum(1, 8)}.png` },
+    offer: {
+      title: 'myApartments',
+      price: getRandomWholeNum(100, 1000),
+      photos: generatePhotos(),
+      address: `${location.lat}, ${location.lng}`,
+      types: generateRandomArrayItem(propertyTypes),
+      rooms: getRandomWholeNum(1, 3),
+      guests: getRandomWholeNum(1, 5),
+      checkin: generateRandomArrayItem(timeType),
+      checkout: generateRandomArrayItem(timeType),
+      description: 'propertyDescription',
+      features: generateFeatures(),
+    },
+    location: {
+      lat: location.x,
+      lng: location.y,
+    },
+  };
+}
+
+function getUsersList(countUsers) {
   const usersList = [];
-  for (let variable = 0; variable < numberUsers; variable++) {
-    // eslint-disable-next-line no-use-before-define
+  for (let variable = 0; variable < countUsers; variable++) {
     usersList.push(generateUser());
   }
   return usersList;
@@ -34,78 +89,3 @@ function getUsersList(numberUsers) {
 
 // eslint-disable-next-line no-console
 console.log(getUsersList(10));
-
-/*
-"string".padStart(targetLength [,padString]):
-const str = "5";
-str.padStart(4, "0"); // "0005"
-*/
-
-function generateUser() {
-  const avatar = `img/avatars/user0${  getRandomWholeNum(1, 8)  }.png`;
-  return {
-    author: { userAvatar: avatar },
-    offer: {
-      title: 'myApartments',
-      price: getRandomFractionNum(100, 1000, 2),
-      // eslint-disable-next-line no-use-before-define
-      photos: generatePhotos(getRandomWholeNum(1, 20)),
-      // eslint-disable-next-line no-useless-concat
-      address: 'location.x' + ' location.y',
-      // eslint-disable-next-line no-use-before-define
-      types: generateHousingType(),
-      rooms: getRandomWholeNum(1, 3),
-      guests: getRandomWholeNum(1, 5),
-      // eslint-disable-next-line no-use-before-define
-      checkin: generateCheckinCheckoutTime(),
-      // eslint-disable-next-line no-use-before-define
-      checkout: generateCheckinCheckoutTime(),
-      description: 'propertyDescription',
-      // eslint-disable-next-line no-use-before-define
-      features: generationFeatures(),
-    },
-    location: {
-      lat: getRandomFractionNum(35.65000, 35.70000, 5),
-      lng: getRandomFractionNum(139.70000, 139.80000, 5),
-    },
-  };
-}
-
-function generatePhotos(numberPhotos) {
-  const templatePhotos = [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-  ];
-  const photos = [];
-  for (let variable = 0; variable < numberPhotos; variable++) {
-    const randomPhoto = getRandomWholeNum(0, 2);
-    photos.push(templatePhotos[randomPhoto]);
-  }
-  return photos;
-}
-
-function generateHousingType() {
-  const propertyTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-  const randomHousing = getRandomWholeNum(0, 4);
-  const houseType = propertyTypes[randomHousing];
-  return houseType;
-}
-
-function generateCheckinCheckoutTime() {
-  const timeType = ['12:00', '13:00', '14:00'];
-  const randomCheckinTime = getRandomWholeNum(0, 1);
-  const сheckinTime = timeType[randomCheckinTime];
-  return сheckinTime;
-}
-
-
-function generationFeatures() {
-  const featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  const features = {};
-  const count = getRandomWholeNum(3, 15);
-  for (let variable = 0; variable < count; variable++) {
-    features[featuresList[getRandomWholeNum(0,5)]] = 'true';
-  }
-  return Object.keys(features);
-}
